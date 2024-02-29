68ee36c63bc743504bdcdf11861141d6ed9d3cb4
Brian <gisameht@gmail.com>
2024-02-29 02:52:27 -0500

    general simulaciones y analisia estadistico

--- Sim.py
+++ Sim.py
@@ -17,3 +17,34 @@ T = 60  # Tiempo final de la simulación (5 años)
 cantS = 100
 ganancias_maximas_por_simulacion = [0] * cantS
 capital_perdido = [0] * cantS
+
+for j in range(cantS):
+    result = simulate_insurance_risk_model(
+        a0, n0, param_lambda, param_ni, param_mi, c, T, F
+    )
+    (a, fbm, bankruptcy_time, profits, claims, cant_nNew, cant_nOut) = result
+    (profit_fbm, time_fbm) = fbm
+    ganancias_maximas_por_simulacion.append(profit_fbm)
+    for i in range(len(claims)):
+        (cant_claims, amount) = claims[i]
+        capital_perdido[j] += cant_claims
+
+
+# Análisis estadístico
+media_ganancias_maximas = mean(ganancias_maximas_por_simulacion)
+varianza_ganancias_maximas = np.var(ganancias_maximas_por_simulacion)
+media_Capital_perdido_por_reclamación = mean(capital_perdido)
+varianza_Capital_perdido_por_reclamación = np.var(capital_perdido)
+
+
+# Imprimir resultados
+print(f"Media de las ganancias maximas de las simulaciones: {media_ganancias_maximas}")
+print(
+    f"Varianza de las ganancias maximas de las de las simulaciones: {varianza_ganancias_maximas}"
+)
+print(
+    f"Media del costo total de las reclamaciones: {media_Capital_perdido_por_reclamación }"
+)
+print(
+    f"Varianza del costo total de las reclamaciones: {varianza_Capital_perdido_por_reclamación}"
+)
